4c8fc58286449e9da5279cb1399eb442b3ef2044
YuliyaNaletskaya <naletskaya@gmail.com>
2025-01-02 17:59:06 +0300

    feat: add solutions for encode-line and file-names tasks

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const nameCount = {};
+  const result = [];
+
+  names.forEach(name => {
+    if (nameCount[name] === undefined) {
+      nameCount[name] = 0;
+      result.push(name);
+    } else {
+      nameCount[name]++;
+      let newName = `${name}(${nameCount[name]})`;
+      while (nameCount[newName] !== undefined) {
+        nameCount[name]++;
+        newName = `${name}(${nameCount[name]})`;
+      }
+      nameCount[newName] = 0;
+      result.push(newName);
+    }
+  });
+
+  return result;
 }
 
 module.exports = {
