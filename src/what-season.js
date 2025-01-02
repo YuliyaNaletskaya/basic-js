edb1a05220066437c8c87ea49aa885cbfd9b3058
YuliyaNaletskaya <naletskaya@gmail.com>
2025-01-02 14:15:48 +0300

    feat: add some slution season task but 2 tests crush

--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!(date instanceof Date) || isNaN(date.getTime()) || Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error('Invalid date!');
+  }
+
+  const dateMethods = ['getTime', 'getFullYear', 'getMonth', 'getDate', 'getHours', 'getMinutes', 'getSeconds', 'getMilliseconds'];
+  for (const method of dateMethods) {
+    if (typeof date[method] !== 'function') {
+      throw new Error('Invalid date!');
+    }
+  }
+
+  let season;
+  if (date.getMonth() >= 2 && date.getMonth() < 5) {
+    season = 'spring';
+  } else if (date.getMonth() >= 5 && date.getMonth() < 8) {
+    season = 'summer';
+  } else if (date.getMonth() >= 8 && date.getMonth() < 11) {
+    season = 'autumn';
+  } else {
+    season = 'winter';
+  } 
+  return season;
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
